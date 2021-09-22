4a190a989640f05d0cb7a088f5ac03570a75b81e
Sophie <sophiejoliver19+1@gmail.com>
2021-09-22 16:15:47 +0000

    using setTimeout to create basic spinner animation

---
+++ spinner1.js
@@ -0,0 +1,23 @@
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 100);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 300);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 500);
+
+setTimeout(() => {
+  process.stdout.write('\r\\   ');
+}, 700);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 900);
+
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 1100);
